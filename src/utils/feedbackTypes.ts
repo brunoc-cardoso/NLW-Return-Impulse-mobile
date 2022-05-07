617da13858ba329b2750dda4de646f413bb947c0
Bruno Cardoso <brunocpessoal@gmail.com>
2022-05-07 17:34:57 -0300

    add default files to project

---
+++ src/utils/feedbackTypes.ts
@@ -0,0 +1,18 @@
+import BugImg from '@/assets/bug.png';
+import IdeaImg from '@/assets/idea.png';
+import ThoughtImg from '@/assets/thought.png';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: BugImg,
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: IdeaImg,
+  },
+  OTHER: {
+    title: 'Outro',
+    image: ThoughtImg,
+  },
+};
