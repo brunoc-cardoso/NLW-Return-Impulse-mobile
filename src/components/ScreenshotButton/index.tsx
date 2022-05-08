d87159d262aa5df3a6755b703b09a8e673d2b40d
Bruno Cardoso <brunocpessoal@gmail.com>
2022-05-08 19:29:26 -0300

    add ScreenshotButton component

---
+++ src/components/ScreenshotButton/index.tsx
@@ -0,0 +1,40 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+
+import { Camera, Trash } from 'phosphor-react-native';
+
+import { styles } from '@/components/ScreenshotButton/styles';
+
+import { theme } from '@/global/theme';
+
+type ScreenshotButtonProps = {
+  screenshot: string | null;
+  onTakeShot: () => void;
+  onRemoveShot: () => void;
+};
+
+export function ScreenshotButton({
+  screenshot,
+  onTakeShot,
+  onRemoveShot,
+}: ScreenshotButtonProps) {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={screenshot ? onRemoveShot : onTakeShot}
+    >
+      {screenshot ? (
+        <View>
+          <Image source={{ uri: screenshot }} style={styles.image} />
+          <Trash
+            size={22}
+            color={theme.colors.text_secondary}
+            weight="fill"
+            style={styles.removeIcon}
+          />
+        </View>
+      ) : (
+        <Camera size={24} color={theme.colors.text_secondary} weight="bold" />
+      )}
+    </TouchableOpacity>
+  );
+}
